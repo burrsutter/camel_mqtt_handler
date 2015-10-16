4140202e6a7306ca7f97e57b9d80eae4a18cba62
Burr Sutter <bsutter@redhat.com>
2015-10-16 17:37:35 -0400

    unmarshall to the rescue

--- src/main/java/com/redhat/summit/TempHandler.java
+++ src/main/java/com/redhat/summit/TempHandler.java
@@ -11,11 +11,15 @@ public class TempHandler {
     @Handler
     public void process(final Exchange exchange) {
         final Message message = exchange.getIn();
-		System.out.println("What is it: " + message.getBody().getClass().getCanonicalName());
-        // final String text = message.getBody(String.class);
-		final TempPojo temp = message.getBody(TempPojo.class);
-		if (temp != null) {
-			System.out.println(temp.getTemp());
+		// System.out.println("What is it: " + message.getBody().getClass().getCanonicalName());
+		final TempPojo sensorReading = message.getBody(TempPojo.class);
+		if (sensorReading != null) {
+			System.out.println("TEMP: " + sensorReading.getTemp());
+			if (sensorReading.getTemp() > 22.0) {
+				System.out.println("\nHOT\n");
+			}
+		} else {
+			System.out.println("temp is null");
 		}
     }
 
--- src/main/java/com/redhat/summit/TempPojo.java
+++ src/main/java/com/redhat/summit/TempPojo.java
@@ -3,28 +3,25 @@ package com.redhat.summit;
 // {"sensorid":"ti_cc2650", "temp":17.1, "time":1445023872175} 
 public class TempPojo {
   private String sensorid;
-  private String temp;
+  private float temp;
   private String time;
 
   public String getSensorid() {
 	return sensorid;
   }	
   public void setSensorid(String sensorid) {
-	System.out.println(sensorid);
 	this.sensorid = sensorid;
   }
-  public String getTemp() {
+  public float getTemp() {
 	return temp;
   }
-  public void setTemp(String temp) {
-	System.out.println(temp);
+  public void setTemp(float temp) {
 	this.temp = temp;
   }
   public String getTime() {
 	return time;
   }
   public void setTime(String time) {
-	System.out.println(time);
 	this.time = time;
   }
 	
